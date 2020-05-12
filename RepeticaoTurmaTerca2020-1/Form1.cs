b3373deaeccfd2e54bd81e8ac3aea476b9a12d33
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-12 20:36:17 -0300

    Final Commit

--- RepeticaoTurmaTerca2020-1/Form1.cs
+++ RepeticaoTurmaTerca2020-1/Form1.cs
@@ -10,11 +10,78 @@ using System.Windows.Forms;
 
 namespace RepeticaoTurmaTerca2020_1
 {
-    public partial class Form1 : Form
+    public partial class FrmNumeros : Form
     {
-        public Form1()
+        public FrmNumeros()
         {
             InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            // Limpando o ListBox
+            lbNumeros.Items.Clear();
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            // Limpando o ListBox
+            lbNumeros.Items.Clear();
+            // usando o laço for para preencher o ListBox
+            for (int i = 0; i <= 100; i++)
+            {
+                // Checando se o índice atual é par
+                if (i % 2 == 0)
+                {
+                    // Adicionando o Item ao ListBox
+                    lbNumeros.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            // Limpando o ListBox
+            lbNumeros.Items.Clear();
+            // Criando a variável de controle
+            int i = 0;
+
+            // Verificar com while a repetição
+            while (i <= 100)
+            {
+                // verificar se o i é par
+                if (i % 2 == 0)
+                {
+                    // Adicionando o Item ao ListBox
+                    lbNumeros.Items.Add(i.ToString());
+                }
+                // incrementando a variável de controle
+                i++;
+            }
+
+
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            // Limpando o ListBox
+            lbNumeros.Items.Clear();
+            // Criando a variável de controle
+            int i = 0;
+
+            // executando a repetição com do-while
+            do
+            {
+                // verificar se o i é par
+                if (i % 2 == 0)
+                {
+                    // Adicionando o Item ao ListBox
+                    lbNumeros.Items.Add(i.ToString());
+                }
+                // incrementando a variável de controle
+                i++;
+            } while (i <= 100);
+
         }
     }
 }
